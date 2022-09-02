f049157cfbdd19e38a7ff5f0a58fd125cd28277f
ohmyungseok0416 <codeplay.oms@gmail.com>
2022-09-02 19:00:09 +0900

    9/2 남극비버팀 3회차 작업완료. 기본 데이터 업로드 끝. 엔딩 몇개 남음

--- project/game_main.py
+++ project/game_main.py
@@ -1,13 +1,22 @@
 from text_data_01 import questions_01
+from text_data_02 import questions_02
+from text_data_03 import questions_03
+
+import os
+clear = lambda: os.system('cls')
+
 
 # 3명의 인물 선택지
 women = ["선배", "동갑", "후배"]
+
 #인물도전 진행상황 변수
 score1 = 0 # 인물별 연애점수
 score2 = 0 # 인물별 연애점수
 score3 = 0 # 인물별 연애점수
 
+# 10개의 반복질문 함수
 def question10(woman):
+    clear() 
     for question in woman:
         for i in question[0]:
             print(i)
@@ -20,9 +29,11 @@ def question10(woman):
         print(f"상대방 : {question[2][sel-1]}")
         global score1
         score1 += question[3][sel-1]
+        clear()
         # print(score)
 
 running = True
+
 while running:
     #누구와 연애를 할지 선택
     if len(women) > 0:
@@ -34,14 +45,15 @@ while running:
        if chosen_woman == "선배":
             question10(questions_01)
        elif chosen_woman == "동갑":
-            question10(questions_01)
+            question10(questions_02)
        elif chosen_woman == "후배":
-            question10(questions_01)
+            question10(questions_03)
+
     else:
-             print("세명의 여자와 모두 사귀게된 홍식. 그는 쓰레기로 낙인찍혀 영원히 사회에서 격리된다....")
+         print("세명의 여자와 모두 사귀게된 홍최. 그는 쓰레기로 낙인찍혀 영원히 사회에서 격리된다....")
+         running = False
         
 
-    question10(questions_01)
     
     
     
